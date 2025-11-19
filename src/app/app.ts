591c678a2217e0718fe4ef27ba6b5d03013681e9
suraj subhash gupta <suraj.gupta@dealmoney.in>
2025-11-19 12:33:33 +0530

    all setups are done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ZardButtonComponent } from '@shared/components/button/button.component';
+import { buttonVariants } from '@shared/components/button/button.variants';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ZardButtonComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
