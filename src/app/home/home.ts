dfdd198193072233b86ce62b9a30d6f821d7eb63
Saifkhan77806 <saifkhan77806@gmail.com>
2025-11-20 16:35:14 +0530

    custom infinite carasuel is ready with the features of while hover it will paused

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { Hero } from "../hero/hero";
+import { Hero } from '../hero/hero';
+import { About } from '../about/about';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, About],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
